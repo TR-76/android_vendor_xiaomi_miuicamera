8d26993e2a8d25dd1611a93f932c67687495e4c2
HinohArata <mmgcntk@gmail.com>
2025-10-06 19:34:45 +0700

    camera: Sync with tree
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- surya-camera.mk
+++ surya-camera.mk
@@ -30,8 +30,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.miui.notch=1
 
 PRODUCT_SYSTEM_PROPERTIES += \
-    persist.vendor.camera.privapp.list=com.android.camera \
-    vendor.camera.aux.packagelist=com.android.camera
+    persist.vendor.camera.privapp.list=com.android.camera
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(MIUICAMERA_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml
